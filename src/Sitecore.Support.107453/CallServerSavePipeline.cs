d5afad55a12460e6e248e7e0200cc2ee4e88144e
DK\ALTU <altu@sitecore.net>
2017-04-27 21:52:29 +0300

    Patch 107453 iteration one

---
+++ src/Sitecore.Support.107453/CallServerSavePipeline.cs
@@ -0,0 +1,33 @@
+﻿namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.SaveItem
+{
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.ExperienceEditor.Speak.Server.Requests;
+    using Sitecore.ExperienceEditor.Speak.Server.Responses;
+    using Sitecore.ExperienceEditor.Switchers;
+    using Sitecore.Globalization;
+    using Sitecore.Pipelines;
+    using Sitecore.Pipelines.Save;
+
+    public class CallServerSavePipeline : PipelineProcessorRequest<Sitecore.ExperienceEditor.Speak.Server.Contexts.PageContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            PipelineProcessorResponseValue value3;
+            using (new EnforceVersionPresenceDisabler())
+            {
+                PipelineProcessorResponseValue value2 = new PipelineProcessorResponseValue();
+                Pipeline pipeline = PipelineFactory.GetPipeline("saveUI");
+                pipeline.ID = ShortID.Encode(ID.NewID);
+                SaveArgs saveArgs = base.RequestContext.GetSaveArgs();
+                using (new ClientDatabaseSwitcher(base.RequestContext.Item.Database))
+                {
+                    pipeline.Start(saveArgs);
+                    value2.AbortMessage = Translate.Text(saveArgs.Error);
+                    value3 = value2;
+                }
+            }
+            return value3;
+        }
+    }
+}
---
+++ src/Sitecore.Support.107453/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.107453")]
+[assembly: AssemblyProduct("Sitecore.Support.107453")]
+[assembly: ComVisible(false)]
